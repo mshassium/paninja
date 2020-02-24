517cc41c756ae16b78453205ce8ab2f07fdb9846
rakh1213 <rail.khamitov@netcracker.com>
2020-02-24 09:24:18 -0300

    create game window (full screen) and prepare main hero sprite (draft)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use ggez::*;
+
+struct PaninjaGameState {}
+
+impl event::EventHandler for PaninjaGameState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        Ok(())
+    }
+
+    fn draw(&mut self, _ctx: &mut Context) -> GameResult {
+        Ok(())
+    }
+}
+
+fn main() -> GameResult {
+    let game_builder = ggez::ContextBuilder::new("paninja", "mshassium");
+    let (context, event_loop) = &mut game_builder
+        .window_mode(conf::WindowMode::default()
+                    .fullscreen_type(conf::FullscreenType::True))
+        .build()?;
+    println!("Context: {:?}, Event Loop: {:?}", context, event_loop);
+    let game_state = &mut PaninjaGameState {};
+    event::run(context, event_loop, game_state)?;
+    Ok(())
 }
